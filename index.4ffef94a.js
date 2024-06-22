c9738962f9a1fd183cda09d354b62ada648ae225
Pavlo Svinarchuk <shokkontent2004@gmail.com>
2024-06-22 17:24:18 +0300

    make build

---
+++ index.4ffef94a.js
@@ -0,0 +1,2 @@
+!function e(n,t){for(var r in t){var a=document.createElement("ul"),l=document.createElement("li");"object"==typeof t[r]&&null!==t[r]&&(n.appendChild(a),l.textContent=r,a.appendChild(l),e(a,t[r]))}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.4ffef94a.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,33 @@
+"use strict";
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+const tree = document.querySelector("#tree");
+function createTree(element, data) {
+    for(const key in data){
+        const ul = document.createElement("ul");
+        const li = document.createElement("li");
+        if (typeof data[key] === "object" && data[key] !== null) {
+            element.appendChild(ul);
+            li.textContent = key;
+            ul.appendChild(li);
+            createTree(ul, data[key]);
+        }
+    }
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
